0f9d18c54ad6dec38276189542ed6e9be0d05ac1
ahmetpalavan <ahmetpalavann@gmail.com>
2024-07-29 21:09:09 +0300

    chore: Update organization sidebar with logo and link to dashboard

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,10 +1,25 @@
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { ModeToggle } from '~/components/theme';
+import { cn } from '~/lib/utils';
+import logo from '~/public/logo.png';
 
+const font = Poppins({
+  weight: ['600', '700'],
+  subsets: ['latin-ext'],
+});
 export const OrgSidebar = () => {
   return (
-    <div className='hidden lg:flex flex-col space-y-6 w-[200px] pl-5 bg-red-100 pt-5'>
+    <div className='hidden lg:flex flex-col space-y-6 w-[200px] pl-5 pt-5'>
       <ModeToggle />
+      <Link href='/'>
+        <div className='flex items-center gap-x-2'>
+          <Image src={logo} alt='logo' width={24} height={24} className='rounded-full' />
+          <p className={cn('text-sm font-semibold', font.className)}>Dashboard</p>
+        </div>
+      </Link>
     </div>
   );
 };
