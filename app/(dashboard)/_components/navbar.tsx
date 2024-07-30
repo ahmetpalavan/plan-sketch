008f44ef6ebbac0eac4805f9286520b14dc21496
ahmetpalavan <ahmetpalavann@gmail.com>
2024-07-30 23:47:47 +0300

    refactor: Remove unnecessary background color from navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/nextjs';
 
 export const Navbar = () => {
   return (
-    <div className='flex items-center gap-x-4 p-5 bg-green-600'>
+    <div className='flex items-center gap-x-4 p-5'>
       <div className='hidden lg:flex lg:flex-1'>Navbar</div>
       <UserButton />
     </div>
