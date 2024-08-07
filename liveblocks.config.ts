953f3779a15d2087e21af47daa91758a9c3b1de8
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-08-07 21:02:56 +0300

    feat: Added new Liveblocks configuration file and updated .gitignore to exclude /app/api directory.

---
+++ liveblocks.config.ts
@@ -0,0 +1,59 @@
+import { createClient, LiveList, LiveMap, LiveObject } from '@liveblocks/client';
+import { createLiveblocksContext, createRoomContext } from '@liveblocks/react';
+import { Layer } from './types/canvas';
+
+const client = createClient({
+  authEndpoint: '/api/liveblocks-auth',
+  throttle: 16,
+});
+
+// Define Liveblocks types for your application
+// https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
+declare global {
+  interface Liveblocks {
+    // Each user's Presence, for useMyPresence, useOthers, etc.
+    Presence: {
+      cursor: { x: number; y: number } | null;
+      selection: string[];
+    };
+
+    // The Storage tree for the room, for useMutation, useStorage, etc.
+    Storage: {
+      layers: LiveMap<string, LiveObject<Layer>>;
+      layerIds: LiveList<string>;
+    };
+
+    // Custom user info set when authenticating with a secret key
+    UserMeta: {
+      id?: string;
+      info?: {
+        name?: string;
+        picture?: string;
+      };
+    };
+
+    // Custom events, for useBroadcastEvent, useEventListener
+    RoomEvent: {};
+    // Example has two events, using a union
+    // | { type: "PLAY" }
+    // | { type: "REACTION"; emoji: "🔥" };
+
+    // Custom metadata set on threads, for useThreads, useCreateThread, etc.
+    ThreadMetadata: {
+      // Example, attaching coordinates to a thread
+      // x: number;
+      // y: number;
+    };
+
+    // Custom room info set with resolveRoomsInfo, for useRoomInfo
+    RoomInfo: {
+      // Example, rooms with a title and url
+      // title: string;
+      // url: string;
+    };
+  }
+}
+
+export const { RoomProvider, useOthersConnectionIds, useOther, useSelf, useOthers, useMutation, useStorage } = createRoomContext(client);
+
+export const { LiveblocksProvider, useInboxNotifications } = createLiveblocksContext(client);
