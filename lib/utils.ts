ee2abfdb42289e2749f6f72ad6c0bc19821f5fa0
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-08-08 01:38:48 +0300

    feat: Added new function `resizeBounds` to handle resizing of bounds based on corner, initial bounds, and a point in the canvas.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import React, { useCallback } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { Camera, Color } from '~/types/canvas';
+import { Camera, Color, Point, Side, XYWH } from '~/types/canvas';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -22,4 +22,35 @@ export function pointerEventToCanvasPoint(e: React.PointerEvent, camera: Camera)
 
 export function colorToCss(color: Color) {
   return `#${color.r.toString(16).padStart(2, '0')}${color.g.toString(16).padStart(2, '0')}${color.b.toString(16).padStart(2, '0')}`;
+}
+
+export function resizeBounds(corner: Side, initialBounds: XYWH, point: Point) {
+  const result = {
+    x: initialBounds.x,
+    y: initialBounds.y,
+    width: initialBounds.width,
+    height: initialBounds.height,
+  };
+
+  if ((corner && Side.Left) === Side.Left) {
+    result.x = Math.min(point.x, initialBounds.x + initialBounds.width);
+    result.width = Math.abs(point.x - (initialBounds.x + initialBounds.width));
+  }
+
+  if ((corner && Side.Top) === Side.Top) {
+    result.y = Math.min(point.y, initialBounds.y + initialBounds.height);
+    result.height = Math.abs(point.y - (initialBounds.y + initialBounds.height));
+  }
+
+  if ((corner && Side.Right) === Side.Right) {
+    result.x = Math.min(point.x, initialBounds.x);
+    result.width = Math.abs(point.x - initialBounds.x);
+  }
+
+  if ((corner && Side.Bottom) === Side.Bottom) {
+    result.y = Math.min(point.y, initialBounds.y);
+    result.height = Math.abs(point.y - initialBounds.y);
+  }
+
+  return result;
 }
